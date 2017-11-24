fd43423b92aa31d3669fffb4b7cd5d4d83c0e819
Sekret K <boyko.constantine@gmail.com>
2017-11-24 14:38:28 +0300

    inited tables

--- alcotrade/AlcoTrade.DataBase/Version1/1.1.1410_InitTable.cs
+++ alcotrade/AlcoTrade.DataBase/Version1/1.1.1410_InitTable.cs
@@ -6,6 +6,27 @@ namespace AlcoTrade.DataBase.Version1
     {
         public override void Up()
         {
+            CreateTable("Goods", c => new
+            {
+                Id = c.Int(nullable: false, identity: true),
+                Name = c.String(nullable: false),
+                GoodId = c.Int(nullable: false)
+            }).PrimaryKey(t => t.Id);
+
+            CreateTable("GoodGroups", c => new
+            {
+                Id = c.Int(nullable: false, identity: true),
+                Name = c.String(nullable: false),
+            }).PrimaryKey(t => t.Id);
+
+            AddForeignKey("Goods", "GoodId", "GoodGroups", "Id");
+
+            CreateTable("Bottle", c => new
+            {
+                Id = c.Long(nullable: false, identity: true),
+                Name = c.DateTime(nullable: false),
+            }).PrimaryKey(t => t.Id);
+
             
         }
 
